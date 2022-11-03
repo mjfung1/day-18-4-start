0f60921cb5ddab7687c89e7756a1aac106e35bb0
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2022-11-03 02:30:45 +0000

    Add random directions and colors to turtle

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ import random
 tim = t.Turtle()
 
 ########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+
+
+tim.pensize(10)
+
+for _ in range(400):
+  tim.pencolor(random.choice(colors))
+  tim.shape("turtle")
+  tim.setheading(random.choice(directions))
+  tim.forward(20)
