4fb3931a3b5ee31febffa23eb4b0c3df8ad1e9b7
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2022-11-03 02:54:46 +0000

    Add rgb. Make colors more dynamic

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+screen = t.Screen()
+screen.colormode(255)
 
 ########### Challenge 4 - Random Walk ########
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
 
-
+tim.speed("fast")
 tim.pensize(10)
 
-for _ in range(400):
-  tim.pencolor(random.choice(colors))
+for _ in range(4000):
+
+  rgb = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+  tim.pencolor(rgb)
   tim.shape("turtle")
   tim.setheading(random.choice(directions))
   tim.forward(20)
